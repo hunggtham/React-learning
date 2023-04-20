70e5ad734f7ac75f637aea5ffbb3872d9997b542
minho <hunggtham@gmail.com>
2023-04-20 13:44:20 +0900

    init project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="app-container">
+      <header className="App-header">
+        <div>Test</div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
