00e6a6cb2e74cbffe58012a032dcb39d332714aa
minho <hunggtham@gmail.com>
2023-04-20 14:35:43 +0900

    setting route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 // import logo from './logo.svg';
 import './App.css';
 import Header from "./components/Header/Header";
+import { Link } from 'react-router-dom';
 
 function App() {
   return (
     <div className="app-container">
       <header className="App-header">
         <Header />
+        <button><Link to="/users">Go to User Page</Link></button>
+        <button><Link to="/admins">Go to Admin Page</Link></button>
+
       </header>
     </div>
   );
